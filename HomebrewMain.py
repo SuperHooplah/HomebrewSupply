5ea3c544f567bf3bd73daa673d1e8d0d300d3ec6
Emily Hickey <msemilymhickey@gmail.com>
2025-09-02 22:02:40 -0400

    Third commit, Jaccard similarity and Table functional

--- HomebrewMain.py
+++ HomebrewMain.py
@@ -5,6 +5,10 @@ in order to determine what recipes share the most ingredients. Potentially make 
 import sys # used to get csv file
 import csv
 import pandas as pd
+from pandasgui import show
+from itertools import combinations
+from sklearn.metrics import jaccard_score
+import numpy as np
 import matplotlib.pyplot as plt
 
 # global variables
@@ -39,6 +43,33 @@ def main():
     IngredienceDF = pd.read_csv(filename)
 
     print(IngredienceDF.head())
+
+    recipes = IngredienceDF.groupby('Recipe Name')['Name'].apply(set).to_dict()
+
+    recipe_names = sorted(recipes.keys())
+
+    similarity_matrix = pd.DataFrame(index=recipe_names, columns=recipe_names, dtype=float)
+
+    # Compute Jaccard similarity for each pair
+    for recipe1, recipe2 in combinations(recipe_names, 2):
+        ingredients1 = recipes[recipe1]
+        ingredients2 = recipes[recipe2]
+
+        # Jaccard similarity: |A ∩ B| / |A ∪ B|
+        intersection = len(ingredients1 & ingredients2)
+        union = len(ingredients1 | ingredients2)
+        similarity = intersection / union if union > 0 else 0.0
+
+        similarity_matrix.loc[recipe1, recipe2] = similarity
+        similarity_matrix.loc[recipe2, recipe1] = similarity
+
+    # Fill the diagonal with 1s (each recipe is identical to itself)
+    np.fill_diagonal(similarity_matrix.values, -1.0)
+
+'''
+    # open pandasgui for testing purposes. More to come
+    show(similarity_matrix)
+'''
 
 '''
     making a plot with matplotlib.pyplot
