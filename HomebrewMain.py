d482e42f687ea6116c825bc5992527e34c925bf5
Emily Hickey <msemilymhickey@gmail.com>
2025-09-03 21:35:30 -0400

    Fourth Commit - Began implementing the TSP algorithm

--- HomebrewMain.py
+++ HomebrewMain.py
@@ -2,30 +2,14 @@
 in order to determine what recipes share the most ingredients. Potentially make a tree.'''
 
 # imports
-import sys # used to get csv file
-import csv
+
 import pandas as pd
 from pandasgui import show
-from itertools import combinations
-from sklearn.metrics import jaccard_score
+from itertools import combinations, permutations
 import numpy as np
 import matplotlib.pyplot as plt
 
-# global variables
-fields = []  # Column names
-rows = []    # Data rows
-
-
-# - pandas dataframe
-
-
-# - pairwise similarities table
-
-
-# - Decision tree
-
-
-# - Recipe viewer
+# - GUI and Recipe viewer
 
 def main():
     '''
@@ -52,23 +36,69 @@ def main():
 
     # Compute Jaccard similarity for each pair
     for recipe1, recipe2 in combinations(recipe_names, 2):
+        # gets two arrays of the ingredients that make up the recipe
         ingredients1 = recipes[recipe1]
         ingredients2 = recipes[recipe2]
 
         # Jaccard similarity: |A ∩ B| / |A ∪ B|
         intersection = len(ingredients1 & ingredients2)
         union = len(ingredients1 | ingredients2)
-        similarity = intersection / union if union > 0 else 0.0
+        similarity = intersection / union if union > 0 else 0.0 # compact if is easier to think with
 
         similarity_matrix.loc[recipe1, recipe2] = similarity
         similarity_matrix.loc[recipe2, recipe1] = similarity
 
-    # Fill the diagonal with 1s (each recipe is identical to itself)
+    # Fill the diagonal with -1s (each recipe is identical to itself)
     np.fill_diagonal(similarity_matrix.values, -1.0)
 
+    # open pandasgui for testing purposes.
+    # show(similarity_matrix)
+
+    '''So how do we solve the problem of working our way through the entire recipe book with as little waste as
+    possible. Well, that just sounds like a Traveling Salesman problem! For those unaware, the TSP is a common
+    issue in computational complexity where you try to find the lowest cost "route" between certain nodes. In our case,
+    our TSP will use our similarity matrix in order to traverse to the next brew with the highest similarity.
+    
+    My basic idea is as follows
+    We start with the Amber Ale, per the instructions of my friend who I am building this for in the first place.
+    We will then observe all of the values and prioritize the one with the highest similarity (Currently, Lawn Mower)
+    '''
+
 '''
-    # open pandasgui for testing purposes. More to come
-    show(similarity_matrix)
+    recipe_order, value = brute_force_tsp(similarity_matrix)
+    print("Optimal order: " + recipe_order + "\nValue = " + value)
+'''
+
+
+''' 
+    Credit to W3schools for the TSP methods. I've modified them to find the largest value (since higher similarity means
+    more ingredients in common
+'''
+
+'''
+def calculate_distance(route, distances):
+    total_distance = 0
+    for i in range(len(route) - 1):
+        total_distance += distances.loc[route[i], route[i + 1]]
+    total_distance += distances.loc[route[-1], route[0]]
+    return total_distance
+
+ # Time to put my $3000 pc to use lmao
+def brute_force_tsp(distances):
+    n = len(distances)
+    nodes = list(range(1, n))
+    longest_route = None
+    max_distance = float('-inf')
+    for perm in permutations(nodes):
+        current_route = [0] + list(perm)
+        current_distance = calculate_distance(current_route, distances)
+
+        if current_distance > max_distance:
+            max_distance = current_distance
+            longest_route = current_route
+
+    longest_route.append(0)
+    return longest_route, max_distance
 '''
 
 '''
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # HomebrewSupply
-A python application that takes an input from an amateur alcohol homebrewer and gives the optimal order to make the recipes in, accounting for pairwise similarity between recipes and ingredient spoilage,
+A python application that takes an input from an amateur alcohol homebrewer and gives the optimal order to make 
+the recipes in, accounting for pairwise similarity between recipes and ingredient spoilage,
+
+This program was made by an ace woman for her best friend who is also a woman and also gay. 
+Computer Science was built on the backs of gay men and unrecognized women. I will never be half the people they 
+were, but if I don't acknowledge what I am then people can pretend I didn't exist when the history books write
+about this time, like they tried to pretend my grandmothers didn't exist. 
